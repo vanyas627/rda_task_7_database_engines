ce904549f7f53692aa9b050025cb6118820dc4ea
Ivan Sotsenko <vanja.sotsenko@gmail.com>
2026-05-17 13:23:50 +0200

    Change engine in ProductDescription table

--- task.sql
+++ task.sql
@@ -23,7 +23,7 @@ CREATE TABLE ProductDescription (
     CountryID INT,
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE CASCADE,
     PRIMARY KEY (ID)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB;
 
 CREATE TABLE Logs (
     ID INT,
